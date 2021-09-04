9d8795d569010287b480b18b719fce72d9a78a4f
J0Miles <jordanamiles@hotmail.com>
2021-09-04 12:42:23 -0500

    Finalized backend -> frontend integrations

--- backend/controllers/todo.go
+++ backend/controllers/todo.go
@@ -14,13 +14,21 @@ import (
 )
 
 var (
-	id         int
-	title      string
-	completed  bool
-	created_at time.Time
-	view       = template.Must(template.ParseFiles("./views/index.html"))
-	database   = config.Database()
+	id          int
+	title       string
+	description string
+	completed   bool
+	created_at  time.Time
+	view        = template.Must(template.ParseFiles("./views/index.html"))
+	database    = config.Database()
 )
+
+type Todo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Created_at  time.Time
+	complete    bool
+}
 
 func GetAllTodos(w http.ResponseWriter, r *http.Request) {
 	// Query string to get all results
@@ -34,7 +42,7 @@ func GetAllTodos(w http.ResponseWriter, r *http.Request) {
 
 	// Scans for the given table columns from DB
 	for statement.Next() {
-		err = statement.Scan(&id, &title, &created_at, &completed)
+		err = statement.Scan(&id, &title, &description, &created_at, &completed)
 
 		if err != nil {
 			fmt.Println(err)
@@ -42,11 +50,12 @@ func GetAllTodos(w http.ResponseWriter, r *http.Request) {
 
 		// Assigns db values to model
 		todo := models.Todo{
-			Id:         id,
-			Title:      title,
-			Updated_at: time.Now(),
-			Created_at: created_at,
-			Completed:  completed,
+			Id:          id,
+			Title:       title,
+			Description: description,
+			Updated_at:  time.Now(),
+			Created_at:  created_at,
+			Completed:   completed,
 		}
 		todos = append(todos, todo)
 	}
@@ -72,21 +81,54 @@ func GetAllTodos(w http.ResponseWriter, r *http.Request) {
 
 func Add(w http.ResponseWriter, r *http.Request) {
 	// Attempting to insert multiple values into db exec
-	title := r.FormValue("title")
-
 	// Instantiate created_at time
-	createdAt := time.Now()
-
-	_, err := database.Exec(`INSERT INTO todos (title, createdAt) VALUES (?, ?)`, title, createdAt)
-
+	timeNow := time.Now()
+	fmt.Println(timeNow)
+	fmt.Println("APPLES!!!!!!!!!!", r.Body)
+	decoder := json.NewDecoder(r.Body)
+	var t models.Todo
+	err := decoder.Decode(&t)
 	if err != nil {
 		log.Fatal(err)
 	}
-	http.Redirect(w, r, "/", 303)
+
+	stmt, err := database.Prepare("INSERT INTO todos(title, description, created_at) VALUES(?,?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, e := stmt.Exec(t.Title, t.Description, time.Now().Format("2006-01-02 15:04:05"))
+	if e != nil {
+		log.Fatal(e)
+		fmt.Println(e)
+	}
+
+	response, err := json.Marshal(res)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+
+	fmt.Println("RESPONSE!!!!!!!!!", res)
+
+	// Allow Cross-Site Request
+	//	w.Header().Set("Access-Control-Allow-Origin", "*")
+	//	w.Header().Set("Content-Type", "application/json")
+	//(w).Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+	//(w).Header().Set("Content-type", "application/json")
+	//fmt.Println(w)
+	//w.WriteHeader(http.StatusOK)
+	w.Write(response)
+	w.WriteHeader(200)
+
+	fmt.Println("insert id", id)
+	json.NewEncoder(w).Encode(res)
+
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
 	// This works when using postman. I believe on redirect this is getting overwritten by cached task list? Not sure
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
 	vars := mux.Vars(r)
 	id := vars["id"]
 	fmt.Println(id)
@@ -96,8 +138,8 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	http.Redirect(w, r, "/", 303)
+	w.WriteHeader(200)
+	http.Redirect(w, r, "/", 200)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -1,32 +1,52 @@
 import React from "react";
-import { Todo } from "../models/Todo";
+import {Todo} from "../models/Todo";
+import {ReactComponent as DeleteButton} from "../assets/svg/icons8-delete.svg"
 
 export const TodoItem: React.FC<Todo> = ({
+  Id,
   title,
   description,
   created_at,
   updated_at,
-  completed,
+  Completed,
+  toggleComplete,
+  deleteTask
 }) => {
+  let content;
+  content = Completed ? (
+    <>
+      <h3 className="font-bold line-through">{title}</h3>
+      <p className="text-sm line-through">{description}</p>
+    </>
+  ) : (
+    <>
+      <h3 className="font-bold">{title}</h3>
+      <p className="text-sm">{description}</p>
+    </>);
   return (
     <div className="group">
       <article
-        className="flex justify-between p-6 justify-items-center shadow-md bg-white-900 group-hover:bg-blue-500 group-hover:text-white"
-        onClick={() => {
-          console.log("Toggle Completed");
-        }}
+        className="flex justify-between p-6 shadow-md justify-items-center bg-white-900 group-hover:bg-blue-500 group-hover:text-white"
       >
         <section>
-          <h3 className="font-bold">{title}</h3>
-          <p className="text-sm">{description}</p>
+          {content}
         </section>
-        <input
-          className="self-center justify-center"
-          type="checkbox"
-          name="completed"
-          defaultChecked={completed}
-        />
+        <div className="flex items-center content-center self-center justify-center row-end-2">
+          <input
+            className="self-center justify-center w-5 h-5"
+            type="checkbox"
+            name="completed"
+            defaultChecked={Completed}
+            onClick={() => toggleComplete(Id)}
+          />
+          &nbsp;
+          <div className="flex">
+            <button className="items-center justify-center w-5 h-5">
+              <DeleteButton className="w-5 h-5" onClick={() => deleteTask && deleteTask(Id)} />
+            </button>
+          </div>
+        </div>
       </article>
-    </div>
+    </div >
   );
-};
+}
